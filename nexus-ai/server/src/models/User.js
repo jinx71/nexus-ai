1f01b0448fe41e7f79ec8c9eb61b89c180e409d6
jinx71 <karim.sazed@gmail.com>
2021-06-15 17:30:38 +0000

    feat(server): add User model

---
+++ nexus-ai/server/src/models/User.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Name is required'],
+      trim: true,
+      minlength: [2, 'Name must be at least 2 characters'],
+      maxlength: [60, 'Name must be at most 60 characters'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+      lowercase: true,
+      trim: true,
+      match: [/^[^\s@]+@[^\s@]+\.[^\s@]+$/, 'Please provide a valid email'],
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required'],
+      minlength: [8, 'Password must be at least 8 characters'],
+      // never ship the hash to the client by default
+      select: false,
+    },
+  },
+  { timestamps: true }
+);
+
+// Hash password whenever it is set/changed.
+userSchema.pre('save', async function hashPassword(next) {
+  if (!this.isModified('password')) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  return next();
+});
+
+// Compare a plaintext candidate against the stored hash.
+userSchema.methods.matchPassword = async function matchPassword(candidate) {
+  return bcrypt.compare(candidate, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
